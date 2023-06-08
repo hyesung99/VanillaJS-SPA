c52b38c85ef396b7e8b4629ee0f3f7d687daf69f
hyesung99 <4538asd@gmail.com>
2023-06-08 17:26:46 +0900

    현재 path와 일치하는 route history pushState

--- src/static/index.js
+++ src/static/index.js
@@ -4,4 +4,20 @@ const router = async () => {
     { path: "/myPage", view: () => console.log("to my page")},
     { path: "/settings", view: () => console.log("to settings")},
   ]
+  
+  // default는 홈으로;
+  let url = '/';
+
+  for(const route of routes){
+    if(location.pathname === route.path){
+      url = route.path;
+      console.log(route);
+      break;
+    }
+  }
+
+  history.pushState(null, null, url);
 }
+
+document.addEventListener("DOMContentLoaded", router());
+
